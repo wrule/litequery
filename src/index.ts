ca7cb4b595a3e230a2aa2f4f75eaa9e922fd060f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-23 00:16:13 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ async function ERC20Balance(addresses: string[], contractAddress: string, provid
 
 async function main() {
   const addresses = getAddresses(fs.readFileSync('./.addresses.txt', 'utf8'));
-  const provider = new ethers.JsonRpcProvider('https://arbitrum-mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
+  const provider = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
   const k = await ETHBalance(addresses, provider);
   console.log(k);
   // const k = await ERC20Balance(addresses, '0x912CE59144191C1204E64559FE8253a0e49E6548', provider)
