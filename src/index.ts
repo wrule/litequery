0ec39d21d8a0833f3cd340cb20c45c1a858aee3c
jimao <jimao@jimaodeMBP.lan>
2023-05-22 22:02:21 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import 'global-agent/bootstrap';
 import { ethers } from 'ethers';
 
 async function main() {
-  const a = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
+  const a = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
   const b = await a.getBalance('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
-  console.log(ethers.utils.formatEther(b));
+  console.log(ethers.formatEther(b));
 }
 
 main();
