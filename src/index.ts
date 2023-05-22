163c2405f86dbfa06e6ace9323fd49cd2c377db8
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-22 23:20:01 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'global-agent/bootstrap';
 import { ethers } from 'ethers';
-import { Addresses } from './addresses';
+import { getAddresses } from './addresses';
+import fs from 'fs';
 
 async function main() {
+  const addresses = getAddresses(fs.readFileSync('./.addresses.txt', 'utf8'));
   const provider = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
-  const balances = await Promise.all(Addresses.map((address) => provider.getBalance(address)));
+  const balances = await Promise.all(addresses.map((address) => provider.getBalance(address)));
   let sum = 0n;
   balances.forEach((balance) => sum += balance);
   console.log(ethers.formatEther(sum));
