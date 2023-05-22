728d0d84a7665958e20b7e483e57736920dcc52b
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-23 00:15:04 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { ethers } from 'ethers';
 import { getAddresses } from './addresses';
 import fs from 'fs';
 
-async function ethBalance(addresses: string[], provider: ethers.Provider) {
+async function ETHBalance(addresses: string[], provider: ethers.Provider) {
   const balances = await Promise.all(addresses.map((address) => provider.getBalance(address)));
-  let sum = 0n;
-  balances.forEach((balance) => sum += balance);
+  let sum = 0;
+  balances.forEach((balance) => sum += Number(ethers.formatEther(balance)));
   return sum;
 }
 
@@ -21,8 +21,9 @@ async function ERC20Balance(addresses: string[], contractAddress: string, provid
 async function main() {
   const addresses = getAddresses(fs.readFileSync('./.addresses.txt', 'utf8'));
   const provider = new ethers.JsonRpcProvider('https://arbitrum-mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
-  const k = await ERC20Balance(addresses, '0x912CE59144191C1204E64559FE8253a0e49E6548', provider)
-  console.log(ethers.formatUnits(k, 6));
+  const k = await ETHBalance(addresses, provider);
+  console.log(k);
+  // const k = await ERC20Balance(addresses, '0x912CE59144191C1204E64559FE8253a0e49E6548', provider)
 }
 
 main();
