c1ad4037f3952634f2d0d92ace6b24e8a0f3f34a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-22 23:32:23 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import { ethers } from 'ethers';
 import { getAddresses } from './addresses';
 import fs from 'fs';
 
-async function main() {
-  const addresses = getAddresses(fs.readFileSync('./.addresses.txt', 'utf8'));
-  const provider = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
+async function ethBalance(addresses: string[], provider: ethers.Provider) {
   const balances = await Promise.all(addresses.map((address) => provider.getBalance(address)));
   let sum = 0n;
   balances.forEach((balance) => sum += balance);
+  return sum;
+}
+
+async function main() {
+  const addresses = getAddresses(fs.readFileSync('./.addresses.txt', 'utf8'));
+  const provider = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
+  const sum = await ethBalance(addresses, provider);
   console.log(ethers.formatEther(sum));
 }
 
