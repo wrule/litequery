436e47bd40114864daadd65465370d6ec1decb02
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-22 23:01:14 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import 'global-agent/bootstrap';
 import { ethers } from 'ethers';
+import { Addresses } from './addresses';
 
 async function main() {
-  const a = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
-  const b = await a.getBalance('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
-  const c = await a.getBalance('0x9C95B71a6D495E33C502CE7B032280689C79EfF4');
-  console.log(ethers.formatEther(b + c));
+  const provider = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/89d62adb179b4b7a8f028347dd5a1cc5');
+  const balances = await Promise.all(Addresses.map((address) => provider.getBalance(address)));
+  let sum = 0n;
+  balances.forEach((balance) => sum += balance);
+  console.log(ethers.formatEther(sum));
 }
 
 main();
